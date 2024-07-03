61ed0595a6b929ac5ad8c9ac0a20ee4728f3cb68
fanfan <kudlatyicss@gmail.com>
2024-07-04 00:56:32 +0200

    Fixed text

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { TLStoreSnapshot, Tldraw } from 'tldraw';
 import 'tldraw/tldraw.css';
 import _jsonSnapshot from './snapshot.json';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 const jsonSnapshot = _jsonSnapshot as TLStoreSnapshot;
 
 const LOAD_SNAPSHOT_WITH_INITIAL_DATA = true;
 
 export default function App() {
-  const handleGetSnapshot = (editor) => {
-    const snapshot = editor.store.getSnapshot();
-    console.log(snapshot);  // Wyświetla snapshot w konsoli, można to zmodyfikować do innych potrzeb
-  };
 
   useEffect(() => {
     // Dodanie klasy blokującej przewijanie
@@ -65,7 +61,6 @@ export default function App() {
           snapshot={jsonSnapshot}
           onMount={(editor) => {
             window.scrollTo(0, 0);
-            window.myEditor = editor;
             editor.setCurrentTool('laser');
             editor.updateInstanceState({ isReadonly: true });
             console.log("kozak");
@@ -79,19 +74,4 @@ export default function App() {
     );
   }
 
-  return (
-    <div className="tldraw__editor">
-      <Tldraw
-        onMount={(editor) => {
-          window.scrollTo(0, 0);      
-          window.myEditor = editor;
-          editor.store.loadSnapshot(jsonSnapshot);
-          editor.updateInstanceState({canMoveCamera: false})
-          editor.updateInstanceState({ isFocused: false })
-          editor.updateInstanceState({ isFocusMode: false})
-          window.scrollTo(0, 0);
-        }}
-      />
-    </div>
-  );
 }
